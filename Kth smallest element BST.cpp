59f25e34ea85bcd24ba96c37471196083cb61e14
Muskan Punyani <57041968+muskanpunyani@users.noreply.github.com>
2021-01-18 23:47:18 +0530

    Create Kth smallest element BST.cpp

---
+++ Kth smallest element BST.cpp
@@ -0,0 +1,75 @@
+Given a binary search tree, write a function kthSmallest to find the kth smallest element in it.
+
+ 
+
+Example 1:
+
+Input: root = [3,1,4,null,2], k = 1
+   3
+  / \
+ 1   4
+  \
+   2
+Output: 1
+
+Example 2:
+
+Input: root = [5,3,6,2,4,null,null,1], k = 3
+       5
+      / \
+     3   6
+    / \
+   2   4
+  /
+ 1
+Output: 3
+
+Follow up:
+What if the BST is modified (insert/delete operations) often and you need to find the kth smallest frequently? How would you optimize the kthSmallest routine?
+
+ 
+
+Constraints:
+
+    The number of elements of the BST is between 1 to 10^4.
+    You may assume k is always valid, 1 ≤ k ≤ BST's total elements.
+
+Accepted
+486,936
+Submissions
+78
+****************************************
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    //time=0(n)
+    //space=0(k) recursion stack
+    int ans,n;
+    void smallest(TreeNode* root)
+    {
+        if(root==NULL)
+            return;
+        smallest(root->left);
+        n--;
+        if(n==0)   // reached kth elem
+            ans=root->val;
+        
+        smallest(root->right);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        n=k;
+        smallest(root);
+        return ans;
+        
+    }
+};
